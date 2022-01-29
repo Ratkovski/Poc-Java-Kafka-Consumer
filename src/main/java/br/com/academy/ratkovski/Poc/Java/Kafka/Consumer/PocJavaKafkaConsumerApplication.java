281310e3d02633f998598c2432a733a483005e25
Ratkovski <drezcry@hotmail.com>
2022-01-29 17:39:31 -0300

    chore: Codebase

---
+++ src/main/java/br/com/academy/ratkovski/Poc/Java/Kafka/Consumer/PocJavaKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.Poc.Java.Kafka.Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaKafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaKafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/academy/ratkovski/Poc/Java/Kafka/Consumer/PocJavaKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.Poc.Java.Kafka.Consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaKafkaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
